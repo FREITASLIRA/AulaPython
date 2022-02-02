a96f3d5fe373297badb7b400f3c2479ba8bf465c
Freitas Lira <li_raf@hotmail.com>
2022-02-02 20:56:46 -0300

    AulaPython
    Curso Udemy

---
+++ PythonUdemy/Aula1/Aula1.py
@@ -0,0 +1,6 @@
+
+print("106", "926", "684", end= "-""11", sep= ".") # testado end(Fim) e sep(Separador) no código.
+  # O end eu coloco o - antes do número que eu quero
+  # O sep serviu para colocar o ponto
+
+print("\nOlá mundo '...' Olá ")
